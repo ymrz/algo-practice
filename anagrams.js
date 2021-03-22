52176c0116d360b5164ebe4050f8d58d26d1e963
ymrz <ymrz.work@gmail.com>
2021-03-21 20:36:04 -0500

    frequency checked

---
+++ anagrams.js
@@ -0,0 +1,30 @@
+function anagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  let letterCounter = {};
+
+  for (let i = 0; i < str1.length; i++) {
+    let letter = str1[i];
+
+    letterCounter[letter]
+      ? (letterCounter[letter] += 1)
+      : (letterCounter[letter] = 1);
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+    let letter = str2[i];
+
+    if (!letterCounter[letter]) {
+      return false;
+    } else {
+      letterCounter[letter] -= 1;
+    }
+  }
+
+  return true;
+}
+
+//console.log(anagram("", ""));
+console.log(anagram("car", "acr"));
